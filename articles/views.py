34d19cba0544946d2bf0aca21ea72687a36d104d
getromandev <getroman.dev@gmail.com>
2022-03-31 19:44:18 -0400

    add Search Form & Request Data

--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 
 from .models import Article
 
+def article_search_view(request):
+    # print('DIR ==>', dir(request))
+    # print(request.GET)
+    query_dict = request.GET
+    # query = query_dict.get('q')
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    context = {
+        'object': article_obj
+    }
+    return render(request, 'articles/search.html', context=context)
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
