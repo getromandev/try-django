aebe3092795a50d75fccac1d4843c7f548b2fbfc
getromandev <getroman.dev@gmail.com>
2022-05-05 01:36:00 -0400

    add logout view and logic

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from accounts.views import login_view
+from accounts.views import (
+    login_view,
+    logout_view
+)
 from articles.views import (
     article_search_view,
     article_create_view, 
@@ -31,4 +34,5 @@ urlpatterns = [
     path('articles/<int:id>/', article_detail_view),
     path('admin/', admin.site.urls),
     path('login/', login_view),
+    path('logout/', logout_view),
 ]
